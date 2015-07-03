cc79b4e49a55abd2f44c2ec93d3edfb9fe40dd8a
ziyad <ziyadkag@gmail.com>
2015-07-04 00:46:55 +0300

    Seekers home interface page and swift file created and linked

---
+++ Workers/UITBC_SeekerHomePages.swift
@@ -0,0 +1,35 @@
+//
+//  UITBC_SeekerHomePages.swift
+//  Workers
+//
+//  Created by Ziyad Alghoneim on 7/4/15.
+//  Copyright (c) 2015 ZazowPro. All rights reserved.
+//
+
+import UIKit
+
+class UITBC_SeekerHomePages: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
