b4852268971dd5b9102e45e045eaea950a83576d
Aziz Alghunaim <azizkag@gmail.com>
2015-07-04 21:32:41 +0300

    SignUP works with a username and a password.

--- Workers/AppDelegate.swift
+++ Workers/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("gOb2d4EpoG0SBYowWX3nDrrt1dpKaajptMeFSUcy", clientKey: "4MN99bC4luVHbkw5VZTCHIxQq57AgG4PQgpxYtva")
+        
+        PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)        
         return true
     }
 
