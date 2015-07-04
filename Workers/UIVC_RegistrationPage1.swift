2427b74ae0b1c8d97cd4ac92c6e00c2a458177a3
Aziz Alghunaim <azizkag@gmail.com>
2015-07-04 23:15:29 +0300

    login words correctly and signup moved to collected extra information. Base registration done and works here

--- Workers/UIVC_RegistrationPage1.swift
+++ Workers/UIVC_RegistrationPage1.swift
@@ -7,15 +7,115 @@
 //
 
 import UIKit
+import Parse
 
 class UIVC_RegistrationPage1: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var emailAdress: UITextField!
+    @IBOutlet weak var regType: UISegmentedControl!
+    @IBOutlet weak var passwordConfirmation: UITextField!
+
+    
+    @IBAction func signUp(sender: AnyObject) {
+        
+        // Build the terms and conditions alert
+        let alertController = UIAlertController(title: "Agree to terms and conditions",
+            message: "Click I AGREE to signal that you agree to the End User Licence Agreement.",
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        alertController.addAction(UIAlertAction(title: "I AGREE",
+            style: UIAlertActionStyle.Default,
+            handler: { alertController in self.processSignUp()})
+        )
+        alertController.addAction(UIAlertAction(title: "I do NOT agree",
+            style: UIAlertActionStyle.Default,
+            handler: nil)
+        )
+        
+        // Display alert
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func processSignUp() {
+        
+        var userPhoneNumber = phoneNumber.text
+        var userFirstName = firstName.text
+        var userLastName = lastName.text
+        var userEmailAddress = emailAdress.text
+        var userPassword = password.text
+        var userPasswordConfirmation = passwordConfirmation.text
+        var userTypeArray: [String] = ["Worker", "Seeker"]
+        var userType = userTypeArray[regType.selectedSegmentIndex]
+        
+        //check that the password is valid
+        var passIsGood = false
+        if ((userPassword == userPasswordConfirmation) && (userPassword != "")){
+            passIsGood = true
+        }else{
+            self.message.text = "passwords don't match or you left them empty"
+        }
+        
+        
+        //if the user is a seeker, sign him up
+        if ((userType == "Seeker") && passIsGood){
+            // Ensure email is lowercase
+            userEmailAddress = userEmailAddress.lowercaseString
+            
+            // Add email address validation
+            
+            // Start activity indicator
+            activityIndicator.hidden = false
+            activityIndicator.startAnimating()
+            
+            // Create the user
+            var user = PFUser()
+            user.username = userPhoneNumber
+            user.password = userPassword
+            user.email = userEmailAddress
+            user["firstName"] = userFirstName
+            user["lastName"] = userLastName
+            user["userType"] = userType
+            
+            
+            user.signUpInBackgroundWithBlock {
+                (succeeded: Bool, error: NSError?) -> Void in
+                if error == nil {
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.performSegueWithIdentifier("RegSuccessful", sender: self)
+                    }
+                    
+                } else {
+                    
+                    self.activityIndicator.stopAnimating()
+                    
+                    if let message: AnyObject = error!.userInfo!["error"] {
+                        self.message.text = "\(message)"
+                    }
+                }
+            }
+        }else{//the uesr is a worker, move him to the next page
+            //TODO implement worker registration
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        activityIndicator.hidden = true
+        activityIndicator.hidesWhenStopped = true
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
