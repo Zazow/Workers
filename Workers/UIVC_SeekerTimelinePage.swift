a7579bf0817b0210d2fd093c0567367e3723b803
ziyad <ziyadkag@gmail.com>
2015-07-04 16:10:12 +0300

    TabView Controller created for seeker and linked

---
+++ Workers/UIVC_SeekerTimelinePage.swift
@@ -0,0 +1,35 @@
+//
+//  UIVC_SeekerTimelinePage.swift
+//  Workers
+//
+//  Created by Ziyad Alghoneim on 7/4/15.
+//  Copyright (c) 2015 ZazowPro. All rights reserved.
+//
+
+import UIKit
+
+class UIVC_SeekerTimelinePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
